dd2c87bccaefe7310a41795b00386d1875d4e47d
Mariusz Gorski <mariusz.gorski@axbit.com>
2020-02-01 00:02:09 +0100

    Network selection scene implemented
    Network selection scene is starting scene where player start
    a server or connect as a client. This is some skeleton of page
    to filled with network manager implementation.

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartServer()
+    {
+        Debug.Log("Start server");
+    }
+
+    public void StartClient(string ip)
+    {
+        Debug.Log($"Start client {ip}");
+    }
+
+    public string GetIp()
+    {
+        IPHostEntry host;
+        string localIP = "0.0.0.0";
+        host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (IPAddress ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                localIP = ip.ToString();
+                break;
+            }
+        }
+        return localIP;
+    }
+}
---
+++ Assets/Scripts/NetworkSelection/NetworkSelection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkSelectionState {
+
+}
+
+public class NetworkSelection : MonoBehaviour
+{
+    public Text txtIp;
+    public Button startClient;
+    public Button startServer;
+    public InputField ipInput;
+    
+    private NetworkManager networkManager;
+    private NetworkSelectionState networkSelectionState = new NetworkSelectionState();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkManager = gameObject.GetComponent<NetworkManager>();
+        startClient.onClick.AddListener(OnStartClientClick);
+        startServer.onClick.AddListener(OnStartServerClick);
+        InvokeRepeating("ProcessNetworkSelection", 0, 0.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ProcessNetworkSelection()
+    {
+        Debug.Log("ProcessNetworkSelection: 500ms interval");
+        txtIp.text = networkManager.GetIp();
+    }
+
+    void OnStartClientClick()
+    {
+        networkManager.StartClient(ipInput.text);   
+    }
+
+    void OnStartServerClick()
+    {
+        networkManager.StartServer();
+    }
+}
