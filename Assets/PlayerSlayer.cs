c1789777dca7ecf9062a3f2301ac6cb0f6a12e5d
Siara Siarzewski <sephichi@gmail.com>
2020-02-02 00:21:02 +0100

    dodam wszystko pusccto

---
+++ Assets/PlayerSlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerSlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float CHUJ = 1f;
+    public Rigidbody2D mojCHuj;
+    public CapsuleCollider2D gdzieBoliChuj;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 gdzie = new Vector3();
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            gdzie += Vector3.right * CHUJ;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            gdzie += Vector3.left * CHUJ;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            gdzie += Vector3.up * CHUJ;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            gdzie += Vector3.down* CHUJ;
+        }
+        mojCHuj.AddForce(gdzie);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //var gdzietenchuj = collision.attachedRigidbody.position - gdzieBoliChuj.attachedRigidbody.position;
+        //mojCHuj.MovePosition(mojCHuj.position + gdzietenchuj);
+    }
+}
