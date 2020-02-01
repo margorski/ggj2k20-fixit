d38d115f2d2bcc959779c87dfd9db0d843a07c55
Siara Siarzewski <sephichi@gmail.com>
2020-02-01 15:54:05 +0100

    Added: EditorModel.Message for network, EditorModel.AddBox, EditorModel.AddConnection, Test project to test EditorModel

---
+++ Assets/Scripts/FixItEditor/Enums/BoxColor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.FixItEditor.Enums
+{
+    public enum BoxColor
+    {
+        White, //default?
+        Red,
+        Green,
+        Blue,
+        Pink,
+        Brown,
+        Yellow,
+        Silver,
+
+    }
+}
---
+++ Assets/Scripts/FixItEditor/Enums/BoxShape.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.FixItEditor.Enums
+{
+    public enum BoxShape
+    {
+        Circle, //default undifned shape
+        Triangle,
+        Square,
+        LongRecatngle,
+        Star,
+
+    }
+}
