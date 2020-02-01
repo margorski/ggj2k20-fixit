b67739661bc279aab60c7787af9b095ac0057a69
Tomasz Moroz <tomasz.moroz@axbit.com>
2020-02-01 16:08:25 +0100

    mega kod

---
+++ Assets/LevelFinished.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelFinished : MonoBehaviour
+{
+    void OnCollisionEnter(Collision colliderName)
+    {
+        if (colliderName.collider.name == "Player")
+        {
+            Debug.Log("Level Succesful!");
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    bool isMoveRight = true;
+    public bool canJump = false;
+    // Update is called once per frame
+
+    void OnCollisionEnter(Collision colliderName)
+    {
+        if (colliderName.collider.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+
+    void OnCollisionExit(Collision colliderName)
+    {
+        
+        if (colliderName.collider.tag == "Ground")
+        {
+            canJump = false;
+        }
+    }
+    void FixedUpdate()
+    {
+        //rb.AddForce(10000 * Time.deltaTime, 0, 0);
+        if (Input.GetKey("d") /*&& buttonactive*/)
+        {
+            isMoveRight = true;
+            rb.AddForce(1000*Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("space"))
+        {
+            if (canJump)
+            {
+                Debug.Log("dupa");
+                if (isMoveRight)
+                {
+                    rb.AddForce(0, 150f, 0);
+                }
+                else
+                {
+                    rb.AddForce(0, 150f, 0);
+                }
+            }
+        }
+        if (Input.GetKey("a"))
+        {
+            isMoveRight = false;
+            rb.AddForce(-1000 * Time.deltaTime, 0, 0);
+        }
+        
+    }
+}
